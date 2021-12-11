eac1d588a58e72bab0f9921639dde3c794d2e341
Christopher Prohm <mail@cprohm.de>
2021-12-11 17:49:59 +0100

    Build the ctypes example analogous to the pyo3 one

--- python_ctypes/python_usage.py
+++ python_ctypes/python_usage.py
@@ -1,44 +1,14 @@
-import ctypes
+import pytest
 
-import rust_clib
-
-
-def example():
-    params = Params()
-    assert len(params) == 0
-
-    params["hello"] = b"world"
-
-    assert len(params) == 1
-
-    print(params["hello"])
+from rust_clib import Params
 
 
-class Params:
-    def __init__(self):
-        self._ptr = rust_clib.params_new()
+params = Params()
+assert len(params) == 0
 
-    def __del__(self):
-        rust_clib.params_free(self._ptr)
+params["hello"] = b"world"
+assert len(params) == 1
+assert params["hello"] == b"world"
 
-    def __setitem__(self, name: str, data: bytes):
-        res = rust_clib.params_insert(self._ptr, name.encode("utf8"), data, len(data))
-        if res != 0:
-            raise RuntimeError("Error during call to params_insert")
-
-    def __getitem__(self, name: str) -> bytes:
-        encoded_name = name.encode("utf8")
-        param_len = rust_clib.params_param_len(self._ptr, encoded_name)
-        param_data = rust_clib.params_param_data(self._ptr, encoded_name)
-
-        if param_data == None or param_len < 0:
-            raise KeyError(f"Could not find {name}")
-
-        return ctypes.string_at(param_data, param_len)
-
-    def __len__(self) -> int:
-        return rust_clib.params_len(self._ptr)
-
-
-if __name__ == "__main__":
-    example()
+with pytest.raises(KeyError):
+    params["missing"]
--- python_ctypes/rust_clib.py
+++ python_ctypes/rust_clib.py
@@ -1,4 +1,4 @@
-from ctypes import CDLL, c_int64, c_uint64, c_void_p, c_uint8, c_char_p
+from ctypes import CDLL, c_int64, c_uint64, c_void_p, c_uint8, c_char_p, string_at
 from pathlib import Path
 
 lib = CDLL(str(Path(__file__).parent.joinpath("_rust_clib.so").resolve()))
@@ -26,3 +26,29 @@ params_param_len.restype = c_int64
 params_param_data = lib.params_param_data
 params_param_data.argtypes = [c_void_p, c_char_p]
 params_param_data.restype = c_void_p
+
+
+class Params:
+    def __init__(self):
+        self._ptr = params_new()
+
+    def __del__(self):
+        params_free(self._ptr)
+
+    def __setitem__(self, name: str, data: bytes):
+        res = params_insert(self._ptr, name.encode("utf8"), data, len(data))
+        if res != 0:
+            raise RuntimeError("Error during call to params_insert")
+
+    def __getitem__(self, name: str) -> bytes:
+        encoded_name = name.encode("utf8")
+        param_len = params_param_len(self._ptr, encoded_name)
+        param_data = params_param_data(self._ptr, encoded_name)
+
+        if param_data == None or param_len < 0:
+            raise KeyError(f"Could not find {name}")
+
+        return string_at(param_data, param_len)
+
+    def __len__(self) -> int:
+        return params_len(self._ptr)
