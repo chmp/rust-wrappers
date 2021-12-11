6af05725158c2c2774d9dc2bd1983872f611bf7a
Christopher Prohm <mail@cprohm.de>
2021-12-11 17:33:46 +0100

    Add len and __getitem__ to pyo3 wrapper

--- python_pyo3/pyo3_wrapper/src/lib.rs
+++ python_pyo3/pyo3_wrapper/src/lib.rs
@@ -1,4 +1,4 @@
-use pyo3::{buffer::PyBuffer, prelude::*};
+use pyo3::{buffer::PyBuffer, exceptions, prelude::*, types::PyBytes};
 
 use rust_core::Params as CoreParams;
 
@@ -16,9 +16,28 @@ impl Params {
         }
     }
 
+    fn __len__(&self) -> usize {
+        self.params.len()
+    }
+
     fn __setitem__(&mut self, py: Python, name: String, data: PyBuffer<u8>) -> PyResult<()> {
         self.params.insert(name, data.to_vec(py)?);
         Ok(())
+    }
+
+    fn __getitem__(&self, py: Python, name: &str) -> PyResult<PyObject> {
+        let param = match self.params.get(name) {
+            Some(param) => param,
+            None => {
+                return Err(PyErr::new::<exceptions::PyKeyError, _>(format!(
+                    "Could not find {}",
+                    name
+                )))
+            }
+        };
+
+        let param = PyBytes::new(py, param).to_object(py);
+        Ok(param)
     }
 }
 
--- python_pyo3/python_usage.py
+++ python_pyo3/python_usage.py
@@ -1,3 +1,13 @@
+import pytest
+
 from pyo3_wrapper import Params
 
 p = Params()
+assert len(p) == 0
+
+p["hello"] = b"world"
+assert len(p) == 1
+assert p["hello"] == b"world"
+
+with pytest.raises(KeyError):
+    p["missing"]
