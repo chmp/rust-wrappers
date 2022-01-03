632632a74d535b28fedd0ba165428f22ea92ce40
Christopher Prohm <mail@cprohm.de>
2022-01-03 19:06:01 +0100

    Update ctypes example

--- python_ctypes/python_usage.py
+++ python_ctypes/python_usage.py
@@ -5,9 +5,9 @@ from rust_clib import Params
 params = Params()
 assert len(params) == 0
 
-params["hello"] = b"world"
+params["hello"] = b"w\0rld"
 assert len(params) == 1
-assert params["hello"] == b"world"
+assert params["hello"] == b"w\0rld"
 
 with pytest.raises(KeyError):
     params["missing"]
